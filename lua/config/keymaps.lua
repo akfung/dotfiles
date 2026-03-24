cfaafd7402f298a725e34805b32fad62ac215163
akfung <famousafu@gmail.com>
2026-03-24 14:33:20 -0700

    add yank for file paths

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,13 @@
 -- Add any additional keymaps here
 vim.keymap.set({ "v", "n" }, "<C-d>", "<C-d>zz")
 vim.keymap.set({ "v", "n" }, "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "<leader>yf", function()
+    local filepath = vim.fn.expand("%:p")
+    vim.fn.setreg("+", filepath)
+    vim.notify("Copied full path: " .. filepath, vim.log.levels.INFO)
+  end, { desc = "Yank full file path" })
+vim.keymap.set({ "n", "v" }, "<leader>yr", function()
+    local filepath = vim.fn.expand("%")
+    vim.fn.setreg("+", filepath)
+    vim.notify("Copied relative path: " .. filepath, vim.log.levels.INFO)
+  end, { desc = "Yank relative file path" })
